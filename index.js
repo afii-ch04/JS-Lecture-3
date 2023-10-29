bbcb0702c1f6b5c6a9f2867d8e6d32016fe042be
afii-ch04 <nomi.m2k18@gmail.com>
2023-10-29 16:48:48 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// alert("I'll Win Your Trust");
+// a = prompt("enter your name");
+// alert(a);
+// document.write(a)
+
+
+
+// data type
+// "" === String (For message)
+// Number === 767676 
+// boolean === (True,False)
+// BODMAS = (),operation,/,*,+,-
+
+
+
+name1 = prompt("enter your name")
+std_TotalMarks = prompt("enter yur tot marks")
+std_ObtMarks = prompt("enter your obt marks")
+res = (std_ObtMarks /std_TotalMarks) * 100
+alert(name1 + "You have got it" + res + "%");
+document.write(name1 + "You have got it" + res + "%");
+
+
+
+// How to add numbers 
+// a = +prompt("enter 1st num")
+// b = +prompt("other 2nd num")
+// c = (a + b)
+// alert(c)
+// document.write(c)
